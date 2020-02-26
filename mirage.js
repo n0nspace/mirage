9fe44836cd622343a7c50ef692e030ab78850110
Felix <nonspace.dsgn@gmail.com>
2020-02-26 19:22:53 +0100

    Create mirage.js

---
+++ mirage.js
@@ -0,0 +1,78 @@
+/*PAGES*/
+     function openTab(attr, tab) {
+      var page = $(tab + attr);
+
+      if (page.is( ":hidden" ) && attr != "/") {
+        page.siblings(tab).fadeOut(function() {
+          $('#post_wrapper').fadeOut(function() {
+            page.fadeIn(600);
+          });
+        });
+      } else if (attr === "/") {
+        $('#post_wrapper').fadeIn();
+      } else {
+        page.fadeOut(function() {
+          $('#post_wrapper').fadeIn();
+        });
+      }
+      }
+
+     var hash = $.trim(window.location.hash);
+     if (hash) {
+       openTab(hash, '.page_wrapper');
+     }
+
+    $("#icon_nav a").on('click', function() {
+      let attr = $(this).attr('href');
+      openTab(attr, '.page_wrapper');
+    });
+
+$(document).ready(function() {
+
+    /*pxu*/
+      $('.photo-slideshow').pxuPhotoset({
+        'ligthbox'  : true, // uses the default Tumblr lightbox, change to false to use your own
+	    'highRes'   : true, // will use high res images
+	    'rounded'   : 'all', // corners, all or false
+	    'borderRadius' : '20px',
+	    'exif'      : true, // display EXIF data if available (tooltip)
+	    'captions'  : true, // display individual captions on photos (tooltip)
+	    'gutter'    : '10px', // spacing between the images
+	    'photoset'  : '.photo-slideshow', // photoset wrapper
+	    'photoWrap' : '.photo-data', // photo data wrapper (includes photo, icons + exif)
+	    'photo'     : '.pxu-photo' // photo wrap (includes photo only)
+	  }, function() {
+		// callback
+	});
+
+	/*MEDIA QUERIES*/
+	function mediaQs() {
+	  let breakQ = {select:post width} + 200 + 100 + 80 + {text:sidebar width} + 50;
+
+	  if ($(window).width() < breakQ) {
+       $("body").addClass("wrapped");
+      } else {
+       $("body").removeClass("wrapped");
+      }
+	}
+
+	mediaQs()
+
+    window.addEventListener('resize', function(event){
+      mediaQs();
+    });
+
+    /*FAQ ACCORDION*/
+    $(".row .quest").on('click', function() {
+      var reply = $(this).next(".reply");
+      if (reply.is( ":hidden" )) {
+        $(this).find("i").replaceWith('<i class="fas fa-angle-up"></i>');
+        reply.slideDown();
+      }
+      else {
+        $(this).find("i").replaceWith('<i class="fas fa-angle-down"></i>');
+        reply.slideUp();
+      }
+    });
+
+  });/*document ready*/
